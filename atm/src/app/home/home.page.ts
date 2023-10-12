eafc7567db8e905ac75ee4dc72389cd2a982ed90
HoangHa2808 <thanha2808@gmail.com>
2023-10-12 10:43:51 +0700

    Upload

---
+++ atm/src/app/home/home.page.ts
@@ -0,0 +1,43 @@
+import { state } from '@angular/animations';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+  user = {
+    name: 'Đoàn Cao Nhật Hạ',
+    email: "",
+    balance: null
+
+  }
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      // console.log(params)
+      if (params && params["user"]) {
+        this.user = JSON.parse(params['user']);
+        console.log(this.user)
+      }
+    });
+  
+  }
+
+  ngOnInit() {}
+
+  infor(){
+    this.router.navigate(['/infor'])
+
+  }
+
+  withdraw() {
+    this.router.navigate(['/withdraw'])
+  }
+
+}
---
+++ atm/src/app/login/login.module.ts
@@ -0,0 +1,20 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+
+// import { IonicModule } from '@ionic/angular';
+
+// import { LoginPageRoutingModule } from './login-routing.module';
+
+// import { AuthComponentComponent } from './login.page';
+
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     FormsModule,
+//     IonicModule,
+//     LoginPageRoutingModule
+//   ],
+//   declarations: [AuthComponentComponent]
+// })
+// export class LoginPageModule {}
